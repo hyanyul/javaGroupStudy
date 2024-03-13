e8e56b1348c6e4451c1c622d9aa94166d49f7520
hyanyul <tjdwl0808@gmail.com>
2024-03-13 23:51:48 +0900

    Update 진료_순서_정하기.java

--- src/codingTest/day01/진료_순서_정하기.java
+++ src/codingTest/day01/진료_순서_정하기.java
@@ -1,14 +1,28 @@
 package codingTest.day01;
 
+import java.util.Arrays;
+
 public class 진료_순서_정하기 {
     public static int[] solution(int[] emergency) {
-        int[] answer = {};
 
         int max = 0;
         int num = emergency.length;
 
+        int[] answer = new int[num];
+
+        int[] change = new int[num];
         for(int i=0; i<num; i++){
-            
+            change[i] = emergency[i];
+        }
+
+        Arrays.sort(change);    //오름차순 정렬
+
+        for(int i=0; i<num; i++){
+            for(int n : change){
+                if(n == emergency[i]){
+                    answer[i] = change.indexOf(n);
+                }
+            }
         }
 
         return answer;
