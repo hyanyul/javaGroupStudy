4ce6fb9cb95a60dcbc7ab539a0006ca77a2e2032
hyanyul <tjdwl0808@gmail.com>
2024-03-26 17:30:06 +0900

    3/26

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -5,22 +5,31 @@ public class Account {
     private String name;    //고객 이름
     private String ssn; //고객 주민번호
     private String account; //고객 계좌
-    private String password;    //비밀번호
+    private int password;    //비밀번호
     private int balance;    //잔액
 
-    public void deposit(){  //입금
-
+    Account(String name, String ssn, String account, int password, int balance){  //계좌 개설(생성자)
+        this.name = name;
+        this.ssn = ssn;
+        this.account = account;
+        this.password = password;
+        this.balance = balance;
     }
 
-    public void withdraw(){ //출금
-
+    public void accountInfo(){  //계좌개설 확인
+        System.out.printf("[계좌개설]'이름: %s, 주민등록번호: %s, 계좌번호: %s, 비밀번호: %d, 입금 금액: %d원' 계좌가 계" +
+                        "설되었습니다.", name, ssn, account, password, balance);
     }
 
-    public void account(){  //계좌
-
+    public void deposit(int money){  //입금
+        balance += money;
     }
 
-    public void balance(){  //잔액
+    public void withdraw(int money){ //출금
+        balance -= money;
+    }
 
+    public void balanceCheck(){  //잔액 조회
+        System.out.printf("[잔액조회] %s 계좌의 잔액은 %d원 입니다.", account, balance);
     }
 }
--- src/bank/Exe.java
+++ src/bank/Exe.java
@@ -1,5 +1,7 @@
 package bank;
 
+import java.util.*;
+
 public class Exe {
     public static void main(String[] args){
 //        1. 계좌개설
@@ -7,5 +9,52 @@ public class Exe {
 //        3. 출금
 //        4. 잔액조회
 //        5. 종료
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("은행 입출금 관리 시스템을 시작합니다. ");
+
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true){
+            System.out.println("""
+                    ---<메뉴>---
+                    1. 계좌개설
+                    2. 입금
+                    3. 출금
+                    4. 잔액조회
+                    5. 종료""");
+            System.out.print("원하시는 메뉴의 번호를 입력해주세요: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            if(choice == 1){
+
+            } else if (choice == 2) {
+
+            } else if (choice == 3) {
+
+            } else if (choice == 4) {
+
+            } else if (choice == 5) {
+                System.out.println("시스템을 종료합니다.");
+                break;
+            }
+
+
+
+
+        }
+
     }
+
+    public static void accountCreate(String name, String ssn, String account, int password, int balance){   //계좌개설
+        Account a = new Account(name, ssn, account,password, balance);
+        a.accountInfo();
+    }
+
+
 }
